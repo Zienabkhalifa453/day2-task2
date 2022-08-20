25a3a855c5694a50bbf3a9b98ad01579dd47b68a
Zienabkhalifa453 <92791041+Zienabkhalifa453@users.noreply.github.com>
2022-08-20 03:29:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int n, factor = 1, num;
+            Console.Write("Enter numbers");
+            string s = Console.ReadLine();
+            bool integer = int.TryParse(s, out num);
+            if (integer == true)
+            {
+                for (n= 1; n<= num; n++)
+                {
+                    factor = factor * n;
+                }
+            }
+            else
+            {
+                Console.WriteLine("try again");
+            }
+
+            Console.Write(num+ "! = " + factor);
+
+
+
+
+
+        }
+    }
+}
